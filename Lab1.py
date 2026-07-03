5c77f832fb1d3c6e6199d7ae69121570d681fd4e
xPHUMx <144644015+xPHUMx@users.noreply.github.com>
2026-07-03 11:17:25 +0700

    Add function to calculate grade based on scores

--- Lab1.py
+++ Lab1.py
@@ -1,1 +1,20 @@
 
+def calculate_grade(scores):
+    total = 0
+    for score in scores:
+        total = total + score
+    average = total / len(scores)
+    if average >= 80:
+        grade = "A"
+    elif average >= 70:
+        grade = "B"
+    elif average >= 60:
+        grade = "C"
+    elif average >= 50:
+        grade = "D"
+    else:
+        grade = "F"
+    return grade, average
+
+scores = [85, 92, 78, 88, 95]
+print(calculate_grade(scores))
